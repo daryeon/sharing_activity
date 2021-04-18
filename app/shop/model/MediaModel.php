664b4115013ecad997dd90ca15878a577cbe82fc
zerotone <zerotone@protonmail.com>
2021-04-18 10:08:22 +0800

    fix

---
+++ app/shop/model/MediaModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\shop\model;
+
+use think\Model;
+
+class ShopMediaModel extends Model
+{
+    protected $autoWriteTimestamp = true;
+
+    /**
+     * 关联媒体表
+     * @return \think\model\relation\belongsTo
+     */
+    public function medias()
+    {
+        return $this->belongsTo('ShopBlockModel', 'block_id');
+    }
+}
